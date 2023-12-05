de04636a992886084a5699c8507b8285da3c8341
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2023-12-05 13:38:57 +0700

    Button component #3

---
+++ user-management/src/components/Button/button.test.tsx
@@ -0,0 +1,70 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+
+// Components
+import { Button } from '.';
+
+describe('Button Component', () => {
+  test('Should render successfully', () => {
+    const comp = render(<Button>Button</Button>);
+    expect(comp).toBeTruthy();
+  });
+
+  test('Should match snapshot', () => {
+    const comp = render(<Button>Button</Button>);
+    expect(comp).toMatchSnapshot();
+  });
+
+  test('Should render correct variants secondary', () => {
+    render(<Button variant="secondary">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(
+      /bg-gray-100 text-gray-600 transition-colors hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 active:bg-gray-300/gi
+    );
+  });
+
+  test('Should render correct variants danger ', () => {
+    render(<Button variant="danger">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(
+      /bg-red-500 transition-colors hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500 active:bg-red-600/gi
+    );
+  });
+
+  test('Should render correct size sm', () => {
+    render(<Button size="sm">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(/px-2 py-1/gi);
+  });
+
+  test('Should render correct size lg', () => {
+    render(<Button size="lg">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(/px-8 py-3/gi);
+  });
+
+  test('Should render correct rounded sm', () => {
+    render(<Button rounded="sm">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(/rounded/gi);
+  });
+
+  test('Should render correct rounded lg', () => {
+    render(<Button rounded="lg">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(/rounded-lg/gi);
+  });
+
+  test('Should render correct rounded full', () => {
+    render(<Button rounded="full">Button</Button>);
+    const button = screen.getByRole('button');
+    expect(button.getAttribute('class')).toMatch(/rounded-full/gi);
+  });
+
+  test('Should call onClick function', () => {
+    const mockOnClick = jest.fn();
+    render(<Button onClick={mockOnClick}>Button</Button>);
+    const button = screen.getByRole('button');
+    fireEvent.click(button);
+    expect(mockOnClick.mock.calls.length).toBe(1);
+  });
+});
---
+++ user-management/src/components/Button/index.tsx
@@ -0,0 +1,62 @@
+import { ButtonHTMLAttributes } from 'react';
+import { cls } from '@/utils/cls';
+import { clsx } from 'clsx';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'danger';
+  size?: 'sm' | 'md' | 'lg';
+  rounded?: 'sm' | 'md' | 'lg' | 'full';
+  disabled?: boolean;
+}
+
+const classes = {
+  base: 'flex items-center text-sm font-md',
+  disabled: 'cursor-not-allowed opacity-50',
+  rounded: {
+    sm: 'rounded',
+    md: 'rounded-md',
+    lg: 'rounded-lg',
+    full: 'rounded-full'
+  },
+  size: {
+    sm: 'px-2 py-1',
+    md: 'px-4 py-2',
+    lg: 'px-8 py-3'
+  },
+  variant: {
+    primary:
+      'bg-blue-500 transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600',
+    secondary:
+      'bg-gray-100 text-gray-600 transition-colors hover:bg-gray-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-500 active:bg-gray-300',
+    danger:
+      'bg-red-500 transition-colors hover:bg-red-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-500 active:bg-red-600'
+  }
+};
+
+export const Button = ({
+  children,
+  className,
+  variant = 'primary',
+  size = 'md',
+  rounded = 'md',
+  disabled = false,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <button
+      {...rest}
+      disabled={disabled}
+      className={cls(`
+      ${classes.base}
+      ${classes.variant[variant]}
+      ${classes.size[size]}
+      ${classes.rounded[rounded]}
+      ${disabled && classes.disabled}
+      ${className}
+      `)}
+    >
+      {children}
+    </button>
+  );
+};
