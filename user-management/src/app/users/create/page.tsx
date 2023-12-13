fd1c84c178ef60e3f4303f62e224b9327da07806
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2023-12-13 09:37:59 +0700

    Create user UI #16

---
+++ user-management/src/app/users/create/page.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+
+// Components
+import { Breadcrumb } from '@/components/Breadcrumb';
+import { CreateUserForm } from '@/components/Forms/CreateUserForm';
+
+// Constants
+import { ROUTER } from '@/constants/route';
+
+export const metadata: Metadata = {
+  title: 'Create User'
+};
+
+const Page = async () => {
+  return (
+    <main>
+      <Breadcrumb
+        breadcrumbs={[
+          { label: 'Users', href: ROUTER.USER },
+          {
+            label: 'Create User',
+            href: ROUTER.CREATE_USER
+          }
+        ]}
+      />
+      <CreateUserForm />
+    </main>
+  );
+};
+
+export default Page;
---
+++ user-management/src/components/Forms/CreateUserForm/index.tsx
@@ -0,0 +1,148 @@
+'use client';
+
+import Link from 'next/link';
+import { useForm, SubmitHandler, Controller } from 'react-hook-form';
+
+// Components
+import { Button } from '@/components/Button';
+import { FormControl } from '@/components/FormControl';
+
+// Constants
+import { REGEX } from '@/constants/regex';
+
+interface IFormInput {
+  firstName: string;
+  lastName: string;
+  phone: string;
+  dob: string;
+  entryDate: string;
+}
+
+export const CreateUserForm = () => {
+  const {
+    handleSubmit,
+    control,
+    formState: { errors }
+  } = useForm<IFormInput>();
+  const onSubmit: SubmitHandler<IFormInput> = (data) => console.log(data);
+  const newDate = new Date();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="rounded-md border border-gray-200 bg-gray-50 p-4 md:p-6">
+        <div className="grid md:grid-cols-2 md:gap-6">
+          <div className="mb-4">
+            <Controller
+              control={control}
+              name="firstName"
+              rules={{
+                required: 'First name is required',
+                pattern: { value: REGEX.NAME, message: 'Invalid First name' }
+              }}
+              render={({ field: { onChange } }) => (
+                <FormControl
+                  labelText="First name"
+                  placeholder="First name"
+                  id="firstName"
+                  required
+                  error={errors?.firstName ? true : false}
+                  errorText={errors?.firstName?.message}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </div>
+
+          <div className="mb-4">
+            <Controller
+              control={control}
+              name="lastName"
+              rules={{
+                required: 'Last name is required',
+                pattern: { value: REGEX.NAME, message: 'Invalid Last name' }
+              }}
+              render={({ field: { onChange } }) => (
+                <FormControl
+                  labelText="Last name"
+                  placeholder="Last name"
+                  id="lastName"
+                  required
+                  error={errors?.lastName ? true : false}
+                  errorText={errors?.lastName?.message}
+                  onChange={onChange}
+                />
+              )}
+            />
+          </div>
+        </div>
+
+        <div className="mb-4">
+          <Controller
+            control={control}
+            name="phone"
+            rules={{
+              required: 'Phone is required',
+              pattern: { value: REGEX.PHONE, message: 'Invalid Phone number' }
+            }}
+            render={({ field: { onChange } }) => (
+              <FormControl
+                labelText="Phone number"
+                placeholder="123-456-7891"
+                id="phone"
+                required
+                error={errors?.phone ? true : false}
+                errorText={errors?.phone?.message}
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+
+        <div className="mb-4">
+          <Controller
+            control={control}
+            name="dob"
+            render={({ field: { onChange } }) => (
+              <FormControl
+                type="date"
+                defaultValue={newDate.toISOString().substring(0, 10)}
+                max={newDate.toISOString().substring(0, 10)}
+                labelText="Date of Birth"
+                id="dob"
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+
+        <div className="mb-4">
+          <Controller
+            control={control}
+            name="entryDate"
+            render={({ field: { onChange } }) => (
+              <FormControl
+                type="date"
+                defaultValue={newDate.toISOString().substring(0, 10)}
+                min={newDate.toISOString().substring(0, 10)}
+                labelText="Entry Date"
+                id="entry-date"
+                onChange={onChange}
+              />
+            )}
+          />
+        </div>
+
+        <div className="mb-4">{/* TODO: Handle add techstacks to user */}</div>
+      </div>
+      <div className="mt-6 flex justify-end gap-4">
+        <Link
+          href="/users"
+          className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
+        >
+          Cancel
+        </Link>
+        <Button type="submit">Create User</Button>
+      </div>
+    </form>
+  );
+};
