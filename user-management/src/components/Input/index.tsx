6e6ed5a0dd3285b2075ebbac2878e6eab9894d3b
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-12-26 10:39:55 +0700

    feat: format code

--- user-management/src/components/Input/index.tsx
+++ user-management/src/components/Input/index.tsx
@@ -10,7 +10,8 @@ const classes = {
   base: 'bg-transparent block w-full border text-black text-sm font-md rounded-lg p-2.5',
   disabled: 'cursor-not-allowed opacity-50 shadow-inner',
   variant: {
-    primary: 'border-none outline outline-1 outline-offset-1 outline-gray-500 focus:ring-blue-500 focus:outline-2 focus:outline-blue-500 placeholder-gray-400',
+    primary:
+      'border-none outline outline-1 outline-offset-1 outline-gray-500 focus:ring-blue-500 focus:outline-2 focus:outline-blue-500 placeholder-gray-400',
     danger: 'focus:outline-none border-red-600 focus:border-red-600',
     outline: 'border-none outline-none focus:border-none focus:outline-none'
   }
