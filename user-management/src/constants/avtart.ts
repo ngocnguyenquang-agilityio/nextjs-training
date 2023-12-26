b835fa2fc4bfddac5710ba96f1fe10053137098b
ngoc.nguyenquang <ngoc.nguyenquang@asnet.com.vn>
2023-12-26 17:10:28 +0700

    feat: #50 format code

--- user-management/src/constants/avtart.ts
+++ user-management/src/constants/avtart.ts
@@ -1,1 +1,2 @@
-export const DEFAULT_AVATAR = 'https://static-00.iconduck.com/assets.00/avatar-default-symbolic-icon-1024x975-mvv3hk3d.png';
+export const DEFAULT_AVATAR =
+  'https://static-00.iconduck.com/assets.00/avatar-default-symbolic-icon-1024x975-mvv3hk3d.png';
